ffdf76e95a2fde00258199aa257400c09b4b1326
petksks <115067926+petksks@users.noreply.github.com>
2023-05-30 11:11:05 +0200

    add changes to guide

--- app/guide/page.js
+++ app/guide/page.js
@@ -1,7 +1,88 @@
-import Head from 'next/head'
-import Link from 'next/link'
+"use client";
+import React, { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
 
 const Guide = () => {
+  const [guideType, setGuideType] = useState(null);
+
+  const handleClick = (type) => {
+    if (guideType === type) {
+      setGuideType(null);
+    } else {
+      setGuideType(type);
+    }
+  }
+
+  const renderGuide = () => {
+    switch(guideType) {
+      case 'individual':
+        return (
+          <section className="content py-8">
+            <div className="container mx-auto flex flex-col items-center">
+              <ol className="prose prose-lg text-center">
+                <li>
+                  Step 1: Click
+                  <Link
+                    href="https://discord.com/api/oauth2/authorize?client_id=1065134684616536114&permissions=543582321744&scope=bot"
+                    target="_blank"
+                  >
+                    <span style={{ fontWeight: 'bold' }}> here</span>
+                  </Link>{' '}
+                  to add our bot to your server.{' '}
+                </li>
+                <li>
+                  Step 2: Put 5 players into the team radiant and dire channel.
+                </li>
+                <li>
+                  Step 3: Click the lock emoji to lock the teams and start the game.
+                </li>
+                <li>
+                  Step 4: At the end of the game, answer the generated message in the text channel to inform the bot which team won.
+                </li>
+                <li>
+                  Step 5: Thus, your league is created! Click the trophy emoji to see the league statistics and rankings.
+                </li>
+              </ol>
+            </div>
+          </section>
+        );
+      case 'team':
+        return (
+          <section className="content py-8">
+            <div className="container mx-auto flex flex-col items-center">
+              <ol className="prose prose-lg text-center">
+                <li>
+                  Step 1: Click
+                  <Link
+                    href="https://discord.com/api/oauth2/authorize?client_id=1065134684616536114&permissions=543582321744&scope=bot"
+                    target="_blank"
+                  >
+                    <span style={{ fontWeight: 'bold' }}> here</span>
+                  </Link>{' '}
+                  to add our bot to your server.{' '}
+                </li>
+                <li>
+                  Step 2: Put 5 players into the team radiant and dire channel.
+                </li>
+                <li>
+                  Step 3: Click the lock emoji to lock the teams and start the game.
+                </li>
+                <li>
+                  Step 4: At the end of the game, answer the generated message in the text channel to inform the bot which team won.
+                </li>
+                <li>
+                  Step 5: Thus, your league is created! Click the trophy emoji to see the league statistics and rankings.
+                </li>
+              </ol>
+            </div>
+          </section>
+        );
+      default:
+        return null;
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -13,45 +94,26 @@ const Guide = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-brand-bg text-black">
-        <section className="hero py-16 text-center">
+      <section className="hero py-16 text-center">
           <h1 className="text-4xl font-bold mb-4">Guide to Your Startup</h1>
           <p className="text-xl mb-8">
-            Follow these steps to get started with our Dota 2 in-house league
-            bot:
+            Choose the guide that fits your needs:
           </p>
+          <button 
+            onClick={() => handleClick('individual')}
+            className={`bg-accent hover:bg-accent-light text-white font-bold py-2 px-4 rounded mr-4 ${guideType === 'individual' ? 'shadow-glow' : ''}`}
+          >
+            Guide for Individual Stats
+          </button>
+          <button 
+            onClick={() => handleClick('team')}
+            className={`bg-accent hover:bg-accent-light text-white font-bold py-2 px-4 rounded ${guideType === 'team' ? 'border-2 border-blue-500' : ''}`}
+          >
+            Guide for Team Stats
+          </button>
         </section>
 
-        <section className="content py-16">
-          <div className="container mx-auto flex flex-col items-center">
-            <ol className="prose prose-lg text-center">
-              <li>
-                Step 1: Click
-                <Link
-                  href="https://discord.com/api/oauth2/authorize?client_id=1065134684616536114&permissions=543582321744&scope=bot"
-                  target="_blank"
-                >
-                  <span style={{ fontWeight: 'bold' }}> here</span>
-                </Link>{' '}
-                to add our bot to your server.{' '}
-              </li>
-              <li>
-                Step 2: Put 5 players into the team radiant and dire channel.
-              </li>
-              <li>
-                Step 3: Click the lock emoji to lock the teams and start the
-                game.
-              </li>
-              <li>
-                Step 4: At the end of the game, answer the generated message in
-                the text channel to inform the bot which team won.
-              </li>
-              <li>
-                Step 5: Thus, your league is created! Click the trophy emoji to
-                see the league statistics and rankings.
-              </li>
-            </ol>
-          </div>
-        </section>
+        {renderGuide()}
 
         <section className="cta py-16">
           <div className="container mx-auto text-center">
@@ -74,3 +136,4 @@ const Guide = () => {
 }
 
 export default Guide
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,28 @@
 module.exports = {
-  content: ['./**/*.{js,ts,jsx,tsx}'],
-  theme: {
+    mode: 'jit', // add this line
+    content: ['./**/*.{js,ts,jsx,tsx}'],
+    theme: {
       extend: {
-          backgroundColor: {
-              'primary': '#374151',
-              'secondary': '#374151',
-              'accent': '#000000',
-              'brand-bg': '#FCE44D',
-          },
-          textColor: {
-              'primary': '#374151',
-              'secondary': '#374151',
-              'accent': '#000000',
-          },
-          fontFamily: {
-              'brand': ['Comfortaa', 'sans-serif'],
-          },
+        backgroundColor: {
+          'primary': '#374151',
+          'secondary': '#374151',
+          'accent': '#000000',
+          'brand-bg': '#FCE44D',
+        },
+        textColor: {
+          'primary': '#374151',
+          'secondary': '#374151',
+          'accent': '#000000',
+        },
+        fontFamily: {
+          'brand': ['Comfortaa', 'sans-serif'],
+        },
+        boxShadow: { 
+          'glow': '0 0 10px 0 #000000', 
+        },
       },
-  },
-  variants: {},
-  plugins: [],
-};
+    },
+    variants: {},
+    plugins: [],
+  };
+  
